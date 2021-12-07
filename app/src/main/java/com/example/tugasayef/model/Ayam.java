ff4c7d2e3df74201e23ee4df464af3a08f75b5cf
Syarifhidayatulloh27 <hidayatullohsyarif119@gmail.com>
2021-12-07 12:32:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tugasayef/model/Ayam.java
@@ -0,0 +1,10 @@
+package com.example.tugasayef.model;
+
+public class Ayam extends Hewan{
+
+    public Ayam(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Ayam",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/tugasayef/model/Kucing.java
@@ -0,0 +1,10 @@
+package com.example.tugasayef.model;
+
+public class Kucing extends Hewan{
+
+    public Kucing(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kucing",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
